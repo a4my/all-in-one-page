bad305b3e953ca55e5c5606a906a4e8cd1926175
Alex Fourmy <alexfourmy@gmail.com>
2021-06-10 11:04:53 +0200

    Added scripts folders

---
+++ scripts/modules/clock.js
@@ -0,0 +1,11 @@
+// class Clock {
+//     const clock = document.querySelector('.clock__container')
+//     let time = new Date()
+//     let sec = time.getSeconds()
+//     let min = time.getMinutes()
+//     let hour = time.getHours()
+//     clock.textContent = hour + ':' + min + ':' + sec
+// }
+
+
+// export default Clock
