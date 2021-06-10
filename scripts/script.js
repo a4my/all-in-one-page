d224f191cbf440c2cd45bf4748724f9d219f5259
Alex Fourmy <alexfourmy@gmail.com>
2021-06-10 13:12:34 +0200

    Completed display date feature

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,40 @@ setInterval(function() {
 
     clock.textContent = hour + ':' + min + ':' + sec + ' ' + day
 })
+
+
+///////////////////////
+// Time
+
+// setInterval(function() {
+    const dateDisplay = document.querySelector('.date__container')
+    let date = new Date()
+    let day = date.getUTCDay()
+    let number = date.getUTCDate()
+    let month = date.getUTCMonth()
+    let year = date.getUTCFullYear()
+
+    if(day === 1) { day = 'Monday'}
+    if(day === 2) { day = 'Tuesday'}
+    if(day === 3) { day = 'Wednesday'}
+    if(day === 4) { day = 'Thursday'}
+    if(day === 5) { day = 'Friday'}
+    if(day === 6) { day = 'Saturday'}
+    if(day === 7) { day = 'Sunday'}
+    
+    if(month === 0) { month = 'January'}
+    if(month === 1) { month = 'February'}
+    if(month === 2) { month = 'March'}
+    if(month === 3) { month = 'April'}
+    if(month === 4) { month = 'May'}
+    if(month === 5) { month = 'June'}
+    if(month === 6) { month = 'July'}
+    if(month === 7) { month = 'August'}
+    if(month === 8) { month = 'September'}
+    if(month === 9) { month = 'October'}
+    if(month === 10) { month = 'November'}
+    if(month === 11) { month = 'December'}
+
+    dateDisplay.textContent = day + ' ' + number + ' ' + month + ' ' + year
+    console.log(number)
+// })
