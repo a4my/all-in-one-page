1b6e1edb598951c5b645c7ac14af6279342a6636
Alex Fourmy <alexfourmy@gmail.com>
2021-06-18 15:14:43 +0200

    minor changes to accomodate the to do list feature in the js file

--- scripts/modules/todo-list.js
+++ scripts/modules/todo-list.js
@@ -1,13 +1,19 @@
+////////////////////////
+//// To-do List
+
+//Variables
 const tasks = []
 const newTask = document.querySelector('#addTask')
 const addBtn = document.querySelector('#addBtn')
+const delBtn = document.querySelector('.deleteBtn')
 
 let taskList = document.querySelector('.task__list')
 
+
+// Functions
 function addTask() {
     if(newTask.value.trim() != '') {
         tasks.push(newTask.value.trim())
-        console.log(tasks)
         displayTask()
         newTask.value = ''
     }
@@ -18,18 +24,34 @@ function displayTask() {
 
     for (let i = 0; i < tasks.length; i++) {
         taskList.innerHTML += `
-        <div class="task__container">
+        <div id="task-${i}" class="task__container">
             <p>${tasks[i]}</p>
             <div class="btns">
                 <button><i class="fas fa-check"></i></button>
-                <button><i class="fas fa-trash"></i></button>
+                <button class="deleteBtn" onclick="deleteTask()"><i class="fas fa-trash"></i></button>
             </div>
         </div>
         `  
     }  
 }
 
+function deleteTask(index) {
+    // console.log('clicked')
 
+    // for (let index = 0; index < tasks.length; index++) {
+    //     const element = document.getElementById(`task-${index}`)
+    //     // console.log(element)
+    //     // tasks.splice(element[index], 1)
+    //     element.remove(element)
+    //     // displayTask()
+    // }
+    
+    tasks.splice(index, 1)
+    console.log(tasks)
+    displayTask()
+}
+
+//Events
 
 addBtn.addEventListener('click', () => {
     if(tasks.length <= 4 ) {
@@ -38,3 +60,5 @@ addBtn.addEventListener('click', () => {
         newTask.value = 'No more than 5 tasks!'
     }
 })
+
+
