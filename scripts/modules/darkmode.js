113a2dc46af9a2122129b6a7487093634aed58f4
Alex Fourmy <alexfourmy@gmail.com>
2021-06-19 14:12:18 +0200

    completed the dark mode btn functionality

--- scripts/modules/darkmode.js
+++ scripts/modules/darkmode.js
@@ -4,12 +4,16 @@ const darkModeToggle = document.querySelector('#dark-mode-toggle')
 
 const enableDarkMode = () => {
     document.body.classList.add('darkmode')
+    darkModeToggle.querySelector('i').classList.remove('fa-moon')
+    darkModeToggle.querySelector('i').classList.add('fa-sun')
 
     localStorage.setItem('darkMode', 'enabled')
 } 
 
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode')
+    darkModeToggle.querySelector('i').classList.remove('fa-sun')
+    darkModeToggle.querySelector('i').classList.add('fa-moon')
 
     localStorage.setItem('darkMode', null)
 } 
@@ -22,10 +26,10 @@ darkModeToggle.addEventListener('click', () => {
     darkMode = localStorage.getItem('darkMode')
     if(darkMode !== 'enabled') {
         enableDarkMode()
-        console.log(darkMode)
+        // console.log(darkMode)
     } else {
         disableDarkMode()
-        console.log(darkMode)
+        // console.log(darkMode)
     }
 })
 
